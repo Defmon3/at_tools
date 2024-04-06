31d108c0e241d241483c75de208347080b1fba05
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-06 12:30:43 +0200

    changed it to work with a dir instead

--- eid_search_str.py
+++ eid_search_str.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
-"""Extracts entity IDs from a list of CSV files and generates a search string for lighthouse."""
+"""Extracts entity IDs from all files in a directory."""
 import argparse
 import csv
-from typing import Iterable
+import os.path
 
 
-def files_to_parse(file_list: Iterable[str]) -> None:
+def files_to_parse(csv_dir: str) -> None:
     """Extracts entity IDs from a list of CSV files and generates a search string for lighthouse.
 
     Args:
-        :param field: Field to extract from the CSV files.
-        :param file_list: List of CSV file paths.
+        :param csv_dir: Path do directory containing CSV files
     Returns:
         :return: None
 
     """
-    if not file_list:
-        print("No files to parse.")
+    if not os.path.exists(csv_dir):
+        print(f"Directory '{csv_dir}' does not exist.")
         return
 
     entity_ids = set()
-    for file_path in file_list:
-        with open(file_path, newline='') as csvfile:
+    csv_files = [file for file in os.listdir(csv_dir) if file.endswith('.csv')]
+
+    if not csv_files:
+        print(f"No CSV files found in '{csv_dir}'.")
+        return
+
+    for file_name in csv_files:
+        full_path = os.path.join(csv_dir, file_name)
+        with open(full_path, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             rows = list(reader)
             entity_ids.update({row['Entity Id'] for row in rows if row.get('Entity Id')})
@@ -39,9 +45,10 @@ def files_to_parse(file_list: Iterable[str]) -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Combine CSV files with compatible data structures.")
-    parser.add_argument('csv_files', metavar='file', type=str, nargs='+',
-                        help='CSV files to combine')
+    parser = argparse.ArgumentParser(
+        description="Extract entity IDs from CSV files and generate a search string for lighthouse.")
+    parser.add_argument('csv_dir', metavar='directory', type=str,
+                        help='Directory containing CSV files to parse.')
     args = parser.parse_args()
 
-    files_to_parse(args.csv_files)
+    files_to_parse(args.csv_dir)
