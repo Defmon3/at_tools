5a476f5d9715f2400d977dbd0b1388df99a18505
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-14 15:58:05 +0200

    bugfix in zip op

--- zip_op.py
+++ zip_op.py
@@ -1,25 +1,29 @@
 import argparse
-import os
-import shutil
 from pathlib import Path
+from shutil import move
 from zipfile import ZipFile
 
 
-def extract_zipfiles(target: str = '.'):
-    z_files = list(Path(target).glob('*.zip'))
+def extract_zip_files(target: Path):
+    z_files = list(target.glob('*.zip'))
     print(f"Found {len(z_files)} ZIP files")
 
     for zf in z_files:
-        target_dir = Path(zf.stem)
+        target_dir = target / zf.stem
         with ZipFile(zf) as zip_ref:
             target_dir.mkdir(exist_ok=True)
-            zip_ref.extractall(target_dir.name)
-            shutil.move(zf, os.path.join(zf.absolute(), zf))
+            zip_ref.extractall(target_dir)
+            move(zf, target_dir)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Extract ZIP files in the current directory.")
-    parser.add_argument('target', metavar='target', type=str, nargs='?', default='.', help='Target directory')
+def main():
+    parser = argparse.ArgumentParser(description="Extract ZIP files in the specified directory.")
+    parser.add_argument('target', metavar='TARGET', type=Path, nargs='?', default='.',
+                        help='Target directory where ZIP files are located and extracted to')
     args = parser.parse_args()
 
-    extract_zipfiles(args.target)
+    extract_zip_files(args.target)
+
+
+if __name__ == "__main__":
+    main()
