b184be745af86c8525600983d628461d7804aae8
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2024-04-14 15:52:02 +0200

    modified zip_op to take args

--- zip_op.py
+++ zip_op.py
@@ -1,24 +1,12 @@
+import argparse
 import os
 import shutil
 from pathlib import Path
 from zipfile import ZipFile
 
 
-def print_tree(directory, prefix=''):
-    """
-    Recursively prints the tree structure of the given directory.
-    """
-    files = list(directory.iterdir())
-    for i, file in enumerate(files):
-        end = '└──' if i == len(files) - 1 else '├──'
-        print(f"{prefix}{end} {file.name}")
-        if file.is_dir():
-            extension = '    ' if i == len(files) - 1 else '│   '
-            print_tree(file, prefix=prefix + extension)
-
-
-def extract_zipfiles():
-    z_files = list(Path('.').glob('*.zip'))
+def extract_zipfiles(target: str = '.'):
+    z_files = list(Path(target).glob('*.zip'))
     print(f"Found {len(z_files)} ZIP files")
 
     for zf in z_files:
@@ -27,3 +15,11 @@ def extract_zipfiles():
             target_dir.mkdir(exist_ok=True)
             zip_ref.extractall(target_dir.name)
             shutil.move(zf, os.path.join(zf.stem, zf))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Extract ZIP files in the current directory.")
+    parser.add_argument('target', metavar='target', type=str, nargs='?', default='.', help='Target directory')
+    args = parser.parse_args()
+
+    extract_zipfiles(args.target)
